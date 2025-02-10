bdd8830e581e1c30ac144ebbc8f03ad0d39c323b
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-10 11:38:16 +0500

    Added a card view to the content view to show it

--- Hike/ContentView.swift
+++ Hike/ContentView.swift
@@ -10,9 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Image("image-1")
-            .resizable()
-            .scaledToFit()
+        CardView()
+        
     }
 }
 
