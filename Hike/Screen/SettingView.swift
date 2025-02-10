235fe68b0f452f45cb2c1cd0923c3f488c6d4da0
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-10 15:35:48 +0500

    Added a new swift ui file setting view

---
+++ Hike/Screen/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  Hike
+//
+//  Created by Elexoft on 10/02/2025.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
