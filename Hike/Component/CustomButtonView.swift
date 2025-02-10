c69f49c0511abc0bb0adfbc0ddb33ae01e7528ac
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-10 12:20:15 +0500

    Custombutton view work done

--- Hike/Component/CustomButtonView.swift
+++ Hike/Component/CustomButtonView.swift
@@ -21,7 +21,33 @@ struct CustomButtonView: View {
                         startPoint: .top,
                         endPoint: .bottom)
                 )
-        }
+            
+            Circle()
+                .stroke(
+                    LinearGradient(
+                        colors: [
+                            .customGrayLight,
+                            .customGrayMedium
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom),
+                    lineWidth: 4)
+            
+            Image(systemName: "figure.hiking")
+                .fontWeight(.black)
+                .font(.system(size: 30))
+                .foregroundStyle(
+                    LinearGradient(
+                        colors: [
+                            .white,
+                            .customGrayLight,
+                            .customGrayMedium
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom)
+                )
+        } //: ZSTACK
+        .frame(width: 58, height: 58)
     }
 }
 
