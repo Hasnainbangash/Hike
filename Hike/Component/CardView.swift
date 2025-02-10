c57319b46915a4d50bc184a0808c9e94f68c5cf4
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-10 13:03:54 +0500

    Added two varaibles and make the function ro generate the random values for images

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -8,6 +8,19 @@
 import SwiftUI
 
 struct CardView: View {
+    
+    // MARK: - PROPERTIES
+    
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    // MARK: - FUNCTIONS
+    
+    func randomImage() {
+        randomNumber = Int.random(in: 1...5)
+        imageNumber = randomNumber
+    }
+    
     var body: some View {
         ZStack {
             CustomBackgroundView()
