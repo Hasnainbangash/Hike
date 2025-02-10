18e25e3d9d0793b7127cd67c5213fca1752f069e
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-10 16:15:58 +0500

    Added some text to thr setitng view

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -43,6 +43,14 @@ struct SettingsView: View {
             )
             .padding(.top, 8)
             
+            Text("Where can you find \nperfect tracks?")
+                .font(.title2)
+                .fontWeight(.heavy)
+            
+            Text("The hike which looks gorgeous in photos but is even better once you are actually there. The hike that you hope to do again someday. \nFind the best day hikes in the app.")
+                .font(.footnote)
+                .italic()
+            
             // MARK: - SECTION: ICONS
             
             // MARK: - SECTION: ABOUT
