c36fe41cf651bfae5742bce31d3ca08531ef3a36
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-10 12:57:29 +0500

    Mak esome chnages in the gradient button style with meaasuring th steate of the view

--- Hike/Utility/GradientButtonStyle.swift
+++ Hike/Utility/GradientButtonStyle.swift
@@ -16,6 +16,20 @@ struct GradientButton: ButtonStyle {
             .padding(.vertical)
             .padding(.horizontal, 30)
             .background(
+                // Conditional Statement with Nil Coalescing
+                // Condition? A : B
+                configuration.isPressed ?
+                // A: When User pressed the button
+                LinearGradient(
+                    colors: [
+                        .customGrayMedium,
+                        .customGrayLight
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                :
+                // B: When the button is not pressed
                 LinearGradient(
                     colors: [
                         .customGrayLight,
